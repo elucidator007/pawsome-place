473bc3e2161a6c3450c7a3e1d0b9c673c0892325
Ankush Sangwan <ankush2000sangwan@gmail.com>
2024-12-09 22:44:56 +0530

    added css to components and aded a font library - comic-neus

---
+++ app/matching-breed/constants.js
@@ -0,0 +1,15 @@
+export const TRAIT_MAPPINGS = {
+    'social_butterfly_or_lone_wolf_1': ['scene_stealer', 'hide_away', 'distant_observer', 'selective_friendly'],
+    'social_butterfly_or_lone_wolf_2': ['attention_seeker', 'background_sleeper', 'occasional_visitor', 'complete_ignorer'],
+    'social_butterfly_or_lone_wolf_3': ['playmate', 'distant_observer', 'hide_away', 'gentle_giant'],
+    'energy_meter_1': ['active_joiner', 'chaos_creator', 'lazy_observer', 'dignified_watcher'],
+    'energy_meter_2': ['midnight_runner', 'sleep_buddy', 'night_patrol', 'distance_keeper'],
+    'energy_meter_3': ['box_claimer', 'part_chaser', 'supervisor', 'distance_keeper'],
+    'maintenance_manual_1': ['quick_brush', 'spa_lover', 'salon_visitor', 'self_sufficient'],
+    'maintenance_manual_2': ['vacuum_cleaner', 'food_critic', 'schedule_keeper', 'grazer'],
+    'maintenance_manual_3': ['brave_warrior', 'dramatic', 'zen_master', 'gentle_giant'],
+    'home_sweet_home_1': ['plant_respecter', 'leaf_hunter', 'curious_inspector', 'grass_only'],
+    'home_sweet_home_2': ['quiet_companion', 'keyboard_assistant', 'paper_reorganizer', 'occasional_visitor'],
+    'home_sweet_home_3': ['silent_watcher', 'chatty_observer', 'sleepy_ignorer', 'dramatic_commentator']
+  };
+  
